b53c93e28a760ce98d60ebf8dd3bfbfefa940ed7
dcquijano <dariuscumpio@gmail.com>
2025-09-19 12:41:31 +0800

    done final booking

---
+++ AlegriaCanyoneeringWebBooking/AlegriaCanyoneeringWebBooking/ViewModel/BatchBookingViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AlegriaCanyoneeringWebBooking.Models;
+
+namespace AlegriaCanyoneeringWebBooking.ViewModel
+{
+    public class BatchBookingViewModel
+    {
+        public Guest MainGuest { get; set; }
+        public List<Guest> OtherGuests { get; set; }
+    }
+
+}
