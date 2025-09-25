49d75d3924a18c19e17b34786c8e11d1b1eb4ce4
dcquijano <dariuscumpio@gmail.com>
2025-09-25 13:35:19 +0800

    changing email and security token

--- AlegriaCanyoneeringWebBooking/AlegriaCanyoneeringWebBooking/Service/EmailService.cs
+++ AlegriaCanyoneeringWebBooking/AlegriaCanyoneeringWebBooking/Service/EmailService.cs
@@ -7,7 +7,7 @@ public class EmailService
     public async Task SendEmailAsync(string toEmail, string subject, string body)
     {
         var message = new MimeMessage();
-        message.From.Add(new MailboxAddress("Alegria Canyoneering Web Booking", "dariuscumpio@gmail.com")); // Sender email
+        message.From.Add(new MailboxAddress("Alegria Canyoneering Web Booking", "alegriawebbooking@gmail.com")); // Sender email
         message.To.Add(new MailboxAddress("", toEmail)); // Recipient email
         message.Subject = subject;
 
@@ -19,7 +19,7 @@ public class EmailService
         using (var client = new SmtpClient())
         {
             await client.ConnectAsync("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-            await client.AuthenticateAsync("dariuscumpio@gmail.com", "xlbi sbxh tlwb fapj");
+            await client.AuthenticateAsync("alegriawebbooking@gmail.com", "dgzg tmob pymg okri");
             await client.SendAsync(message);
             await client.DisconnectAsync(true);
         }
