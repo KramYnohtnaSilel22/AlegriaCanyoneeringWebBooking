ebde94cdc81c2f980eb14fdb2dd77ac40e7b1f5f
dcquijano <dariuscumpio@gmail.com>
2025-09-29 14:20:34 +0800

    adding update info for each user like super admin, admin, operator.
    -To change what need to change from the user.

---
+++ AlegriaCanyoneeringWebBooking/AlegriaCanyoneeringWebBooking/ViewModel/OperatorUpdateViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlegriaCanyoneeringWebBooking.ViewModel
+{
+    public class OperatorUpdateViewModel
+    {
+        [Required]
+        public int OperatorId { get; set; }
+
+        [Required, StringLength(255)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(255)]
+        public string BusinessName { get; set; } = string.Empty;
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required, StringLength(255)]
+        public string Gender { get; set; } = string.Empty;
+
+        [Required, StringLength(255)]
+        public string Username { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string? EmailAddress { get; set; }
+
+        [Required]
+        public int RoleId { get; set; }
+    }
+}
