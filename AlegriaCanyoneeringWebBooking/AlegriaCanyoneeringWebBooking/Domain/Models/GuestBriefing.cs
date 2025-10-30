101ac0368e4d8459b60df030c221c2bd6583d8c3
DenDev1 <dendopulgo123@gmail.com>
2025-10-30 21:57:19 +0800

    Add Logs Index

---
+++ AlegriaCanyoneeringWebBooking/AlegriaCanyoneeringWebBooking/Domain/Models/GuestBriefing.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AlegriaCanyoneeringWebBooking.Domain.Models
+{
+    [Table("tbl_guestbreifing")]
+    public class GuestBriefing
+    {
+        [Key]
+        public int BGuestID { get; set; }
+
+        public string BWristBondCode { get; set; } = string.Empty;
+
+        public string? BGuestName { get; set; }
+
+        public DateTime? BDateArrival { get; set; }
+
+        public DateTime? BDateDeparture { get; set; }
+
+        public string? BDateCode { get; set; } // stored as Unix timestamp
+
+        public byte[]? BGuestImage { get; set; } // optional image
+    }
+
+}
---
+++ AlegriaCanyoneeringWebBooking/AlegriaCanyoneeringWebBooking/WebUI/Controllers/Logs/GuestBriefingController.cs
@@ -0,0 +1,39 @@
+﻿using AlegriaCanyoneeringWebBooking;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class GuestBriefingController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public GuestBriefingController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: GuestBriefing
+    public async Task<IActionResult> Index()
+    {
+        var guestBriefings = await _context.GuestBriefings.ToListAsync();
+        return View(guestBriefings);
+    }
+
+    // POST: GuestBriefing/DeleteConfirmed/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var guest = await _context.GuestBriefings.FindAsync(id);
+        if (guest != null)
+        {
+            _context.GuestBriefings.Remove(guest);
+            await _context.SaveChangesAsync();
+        }
+        return RedirectToAction(nameof(Index));
+    }
+
+
+
+
+}
+
